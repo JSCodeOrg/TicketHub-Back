c95facf03ad792b8d9631fa084a36f4e44969141
Jhonny <jhony.duque@correounivalle.edu.co>
2025-06-30 19:30:10 -0500

    Commit

--- src/controllers/event.controller.ts
+++ src/controllers/event.controller.ts
@@ -76,7 +76,7 @@ export class EventController {
 
     public async updateEvent(req: Request, res: Response): Promise<void> {
         const eventData: EventDTO = req.body;
-        const eventId: number = Number(req.params.id);  // El ID debe venir en la URL (más limpio)
+        const eventId: number = Number(req.params.id);  
 
         const { authorization } = req.headers;
 
